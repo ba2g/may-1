7e77bcd49aa190387d17dbe09688e0cc2e703677
ba2g <batusukru-gudek@hotmail.com>
2026-05-01 12:52:23 +0300

    first spring boot project from spring initializr

---
+++ src/test/java/com/batu/make_it_easy/MakeItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batu.make_it_easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeItEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
